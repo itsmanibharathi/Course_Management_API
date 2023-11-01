ded9eb065ab30f32606caeb31d2f73af86473df6
Manibharathi <manibharathidct@gmail.com>
2023-11-01 14:27:55 +0530

    setups

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
+// console.log(process.env.MONGO_URL);
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+      console.log("DB Connetion Successfull");
+    })
+    .catch((err) => {
+      console.log(err.message);
+  });
+
+app.get('/api', (req, res) => {
+  res.send('Server is up and running.');
+});
+
+app.listen(port, (err) => {
+    if (err) {
+    console.log(err);
+  } else {
+    console.log(`Server Started Successfully at ${port}.`);
+  }
+});
